e14eed4b221923c9e37df00392c747091780d3b8
Towolawi <niyi.towolawi@bp.com>
2022-07-08 17:56:59 +0100

    Populate map with all mapped numbers

--- src/main/java/ArabicToRomansConverter.java
+++ src/main/java/ArabicToRomansConverter.java
@@ -8,6 +8,17 @@ public class ArabicToRomansConverter {
     static{
         numbersToRomansMap.put(1, "I");
         numbersToRomansMap.put(4, "IV");
+        numbersToRomansMap.put(5, "V");
+        numbersToRomansMap.put(9, "IX");
+        numbersToRomansMap.put(10, "X");
+        numbersToRomansMap.put(40, "XL");
+        numbersToRomansMap.put(50, "L");
+        numbersToRomansMap.put(90, "XC");
+        numbersToRomansMap.put(100, "C");
+        numbersToRomansMap.put(400, "CD");
+        numbersToRomansMap.put(500, "D");
+        numbersToRomansMap.put(900, "CM");
+        numbersToRomansMap.put(1000, "M");
     }
 
     public String toRomanNumerals(int number){
--- src/test/java/ArabicToRomansConverterTest.java
+++ src/test/java/ArabicToRomansConverterTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -23,7 +22,17 @@ class ArabicToRomansConverterTest {
                 Arguments.of(2, "II"),
                 Arguments.of(3, "III"),
                 Arguments.of(4, "IV"),
-                Arguments.of(5, "V")
+                Arguments.of(5, "V"),
+                Arguments.of(9, "IX"),
+                Arguments.of(10, "X"),
+                Arguments.of(40, "XL"),
+                Arguments.of(50, "L"),
+                Arguments.of(90, "XC"),
+                Arguments.of(100, "C"),
+                Arguments.of(400, "CD"),
+                Arguments.of(500, "D"),
+                Arguments.of(900, "CM"),
+                Arguments.of(1000, "M")
         );
     }
 }
