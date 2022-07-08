2081d8d056375220ebfe596cce9cb7c4c01edeef
Towolawi <niyi.towolawi@bp.com>
2022-07-08 17:44:45 +0100

    Convert to ParameterizedTest Pass for 1-3, fail on 4

--- src/main/java/ArabicToRomansConverter.java
+++ src/main/java/ArabicToRomansConverter.java
@@ -1,6 +1,11 @@
 public class ArabicToRomansConverter {
 
     public String toRomanNumerals(int number){
-        return "I";
+
+        StringBuilder romans = new StringBuilder();
+        for(int i = 0; i < number; i++){
+            romans.append("I");
+        }
+        return romans.toString();
     }
 }
--- src/test/java/ArabicToRomansConverterTest.java
+++ src/test/java/ArabicToRomansConverterTest.java
@@ -1,4 +1,9 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -6,13 +11,18 @@ class ArabicToRomansConverterTest {
 
     ArabicToRomansConverter arabicToRomansConverter = new ArabicToRomansConverter();
 
-    @Test
-    void convert1ToRomanNumerals() {
-        assertThat(arabicToRomansConverter.toRomanNumerals(1)).isEqualTo("I");
+    @ParameterizedTest
+    @MethodSource("fromArabicNumerals")
+    void convert1ToRomanNumerals(int number, String roman) {
+        assertThat(arabicToRomansConverter.toRomanNumerals(number)).isEqualTo(roman);
     }
 
-    @Test
-    void convert2ToRomanNumerals() {
-        assertThat(arabicToRomansConverter.toRomanNumerals(2)).isEqualTo("II");
+    private static Stream<Arguments> fromArabicNumerals() {
+        return Stream.of(
+                Arguments.of(1, "I"),
+                Arguments.of(2, "II"),
+                Arguments.of(3, "III"),
+                Arguments.of(4, "IV")
+        );
     }
 }
